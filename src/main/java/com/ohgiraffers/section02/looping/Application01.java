ff19138b840fa1915589da2001e43eba1cf4ed4d
jungmin <tjwjdals0803@naver.com>
2024-02-13 18:07:22 +0900

    2월 13일 [looping] 수업내용

---
+++ src/main/java/com/ohgiraffers/section02/looping/Application01.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section02.looping;
+
+import com.ohgiraffers.section01.conditional.A_if;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+//        a.testSimpleForStatement();
+//        a.testForExample1();
+//        a.testForExample2();
+//        a.testForExample3();
+//        a.testForExample4();
+//        a.printSimpleGugudan();
+//        a.printSimpleGugudan();
+
+        A_nestedFor a2 = new A_nestedFor();
+//        a2.printGugudanFromTwoToNine();
+//        a2.printUpgradeGugudan();
+        a2.printStarInputRowTimes();
+    }
+
+
+
+
+}
